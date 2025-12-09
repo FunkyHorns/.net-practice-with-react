4b18bc28dcefccf4619759c0c6be01dc2b3f4d75
Lance Joshua Hilario <lance.joshua19@gmail.com>
2025-12-10 02:52:14 +0800

    Second commit

---
+++ Domain/Activity.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice.Domain
+{
+    public class Activity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string? Title { get; set; } 
+        public DateTime Date { get; set; }
+        public required string Desciription { get; set; }
+        public required string Category { get; set; }
+        public required string IsCancelled { get; set; }
+
+        // Location details
+        public required string City { get; set; }   
+        public required string Venue { get; set; }
+        public double Latitude { get; set; }
+    }
+}
